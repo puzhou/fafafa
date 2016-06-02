4e493b71337509651802764af277bbfc0ff64b6a
Administrator <dfddffff@163.com>
2016-06-02 13:11:31 +0800

    写了第一行代码

--- app/src/main/java/com/m520it/myapplication/MainActivity.java
+++ app/src/main/java/com/m520it/myapplication/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这里是我写的第一行代码
     }
 }
